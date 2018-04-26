81f123ac524f4fab6a2d01e39e6f27ad283ebd41
Othmani Ali <othmanixali@gmail.com>
2018-04-26 18:20:25 +0100

    Wrapp App with ParallaxProvider in index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { ParallaxProvider } from 'react-scroll-parallax';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const app = () => {
+        return (
+            <ParallaxProvider>
+                <App />
+            </ParallaxProvider>);
+        };
+
+ReactDOM.render(app() , document.getElementById('root'));
 registerServiceWorker();
