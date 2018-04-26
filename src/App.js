9e72607a4711310c20024d696d187ef4e1519cf7
Othmani Ali <othmanixali@gmail.com>
2018-04-26 18:28:28 +0100

    Imp NavBar cmp in App

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import styles from './App.css';
 import BannerLogo from './Components/BannerLogo/BannerLogo';
+import NavBar from './Components/NavBar/NavBar';
+
 class App extends Component {
   render() {
     return (
       <div className={styles.App}>
-        <BannerLogo />
+          <NavBar />
+          <BannerLogo />
       </div>
     );
   }
