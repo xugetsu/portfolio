523a6c147af6b308a0449ae53a2edda46b730842
Othmani Ali <othmanixali@gmail.com>
2018-04-26 18:19:01 +0100

    Imp BannerLogo in App cmp

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import styles from './App.css';
-
+import BannerLogo from './Components/BannerLogo/BannerLogo';
 class App extends Component {
   render() {
     return (
       <div className={styles.App}>
-        <header className={styles.AppHeader}>
-          <img src={logo} className={styles.AppLogo} alt="logo" />
-          <h1 className={styles.AppTitle}>Welcome to React</h1>
-        </header>
-        <p className={styles.AppIntro}>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <BannerLogo />
       </div>
     );
   }
