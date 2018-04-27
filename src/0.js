fe86bf636ff574bf21bf250905203e43a21d0e71
Othmani Ali <othmanixali@gmail.com>
2018-04-27 14:43:32 +0100

    Create an abstract file for new cmps

---
+++ src/0.js
@@ -0,0 +1,25 @@
+import React , {Component} from 'react';
+import React from 'react';
+import style from 'A.css';
+
+const a = () => {
+    return (
+        <div className= {styles.A}>
+
+        </div>
+    );
+}
+
+class A extends Component{
+    render(){
+        return(
+            <div className= {styles.A}>
+
+            </div>
+        );
+    };
+}
+
+export default A;
+export default a;
+
