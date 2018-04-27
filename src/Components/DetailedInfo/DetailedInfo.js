703325d668445fe7a28f1d6e1ef8c03a91ef2b8f
Othmani Ali <othmanixali@gmail.com>
2018-04-27 14:41:14 +0100

    Create detailedInfo cmp

---
+++ src/Components/DetailedInfo/DetailedInfo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './DetailedInfo.css';
+
+const detailedInfo = () => {
+    return (
+        <div className= {styles.DetailedInfo}>
+
+        </div>
+    );
+}
+
+export default detailedInfo;
