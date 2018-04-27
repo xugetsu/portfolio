6e17f092f253dd6de791b53430e31c0db5983564
Othmani Ali <othmanixali@gmail.com>
2018-04-27 14:44:59 +0100

    imp cmps in app

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import logo from './logo.svg';
 import styles from './App.css';
 import BannerLogo from './Components/BannerLogo/BannerLogo';
 import NavBar from './Components/NavBar/NavBar';
-
+import Modules from './Components/modules/Modules';
+import GeneralInfo from './Components/GeneralInfo/GeneralInfo';
+import DetailedInfo from './Components/DetailedInfo/DetailedInfo';
 class App extends Component {
   render() {
     return (
       <div className={styles.App}>
-          <NavBar />
-          <BannerLogo />
+        <NavBar />
+        <GeneralInfo />
+        <DetailedInfo />
+        <Modules />
+        <BannerLogo />
       </div>
     );
   }
