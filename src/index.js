963995941e699d4f8809ac31d6ebf8629a58a0c0
Othmani Ali <othmanixali@gmail.com>
2018-04-27 14:44:11 +0100

    M index style

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { ParallaxProvider } from 'react-scroll-parallax';
+import './index.css';
 
 const app = () => {
         return (
