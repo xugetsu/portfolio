66dca626f37c5a08a618db63b81e8a0940adef5e
Othmani Ali <othmanixali@gmail.com>
2018-04-28 17:57:59 +0100

    Updates GeneralInfo

--- src/Components/GeneralInfo/GeneralInfo.js
+++ src/Components/GeneralInfo/GeneralInfo.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './GeneralInfo.css';
 import myPhoto from '../../Assets/Images/ali.jpg';
+import gear from '../../Assets/Icons/svgs/gear.svg';
 import Col2 from './Col2/Col2';
 import AboutMe from './AboutMe/AboutMe';
 import MyLogoRaw from '../MyLogoRAW/MyLogoRaw';
@@ -14,12 +15,13 @@ const generalInfo = () => {
                         <MyLogoRaw colorFill1 = {'#23262b'} colorFill2 = {'#1f2227'} logoHeight = {900}/>
                 </div>   
                 <img  className= {styles.MyPhoto} src={myPhoto} height={200}/>
+                <img  className= {styles.Gear} src={gear} height={800}/>
                 <ul className= {styles.Info} >
                     <b>{'\{ '}</b>
                     <li><span>" FULL NAME " :</span> " Ali Othmani "<b> ,</b></li>
                     <li><span>" BORN " :</span> " [3, 9, 1992] "<b> ,</b></li>
                     <li><span>" EMAIL " :</span> " othmanixali@gmail.com "<b> ,</b></li>
-                    <li><span>" ADRESS " :</span> " Susah - TN "<b> ,</b></li>
+                    <li><span>" ADDRESS " :</span> " Sousse - TN "<b> ,</b></li>
                     <li><span>" PHONE " :</span> " parseInt('175CC0A', 16) "</li>
                     <b>{' \}'}</b>
                 </ul>
