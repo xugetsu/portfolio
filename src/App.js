148cde115bc681a189f4a436939287fb0922665a
Othmani Ali <othmanixali@gmail.com>
2018-04-28 05:34:35 +0100

    Removing DetailedInfo & Modules cmps from App

--- src/App.js
+++ src/App.js
@@ -5,15 +5,12 @@ import BannerLogo from './Components/BannerLogo/BannerLogo';
 import NavBar from './Components/NavBar/NavBar';
 import Modules from './Components/modules/Modules';
 import GeneralInfo from './Components/GeneralInfo/GeneralInfo';
-import DetailedInfo from './Components/DetailedInfo/DetailedInfo';
 class App extends Component {
   render() {
     return (
       <div className={styles.App}>
         <NavBar />
         <GeneralInfo />
-        <DetailedInfo />
-        <Modules />
         <BannerLogo />
       </div>
     );
