0be53144f802f66898fb090614d02d557ca70e43
Othmani Ali <othmanixali@gmail.com>
2018-04-28 05:30:19 +0100

    Create AboutMe cmp

---
+++ src/Components/GeneralInfo/AboutMe/AboutMe.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './AboutMe.css';
+import LogoFragment1 from '../../LogoFragments/LogoFragment1/LogoFragment1';
+import LogoFragment2 from '../../LogoFragments/LogoFragment2/LogoFragment2';
+import LogoFragment3 from '../../LogoFragments/LogoFragment3/LogoFragment3';
+
+const aboutMe = () => {
+    return (
+        <div className= {styles.AboutMe}>
+            <h1>About Me</h1>
+            <ul>
+                <li><span><LogoFragment2/></span>I'm a React Js web developer and a mechatronic engineer.</li>
+                <li><span><LogoFragment2/></span>I studied at the National School of Engineering of Sousse as a mechatronic engineer and after graduation, I started learning about web development..</li>
+                <li><span><LogoFragment2/></span>I was and still very passionate about web development and I have made a firm decision to keep going in this career path beleiving that someday I can benefit from both of my specialities together !! </li>
+            </ul>
+        </div>
+    );
+}
+
+export default aboutMe;
