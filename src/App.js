77eed88bff9ae486e12f6359ffdaf92bc23c050d
Othmani Ali <othmanixali@gmail.com>
2018-04-29 18:41:19 +0100

    Imp GitMyCareer in App

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import BannerLogo from './Components/BannerLogo/BannerLogo';
 import NavBar from './Components/NavBar/NavBar';
 import Modules from './Components/modules/Modules';
 import GeneralInfo from './Components/GeneralInfo/GeneralInfo';
+import GitMyCareer from './Components/GitMyCareer/GitMyCareer';
 class App extends Component {
   render() {
     return (
       <div className={styles.App}>
         <NavBar />
         <GeneralInfo />
+        <GitMyCareer />
         <BannerLogo />
       </div>
     );
