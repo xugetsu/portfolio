9f953ae26628a71151b483208a08b5ebf88a1387
Othmani Ali <othmanixali@gmail.com>
2018-04-30 21:59:29 +0100

    Merging branch cmp in commit cmp

--- src/Components/GitMyCareer/Commit/Commit.js
+++ src/Components/GitMyCareer/Commit/Commit.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import styles from './Commit.css';
+import CommitStyles from './Commit.css';
+import BranchStyles from './Branch.css';
+import newbranch from '../../../Assets/Icons/svgs/branch.svg';
 
 const commit = (props) =>{
-    const width = props.width,
-          color = props.color;      
-    // const classes = props.branch ? styles.CommitNewBranch : styles.Commit;
-    return ( 
-        <div className= {styles.Commit}  style={{borderBottomColor:color?color:'#ff7b00',width:width?width:'50px'}}>
-             <span onClick = {props.clicked}>
-                 <div  className= {styles.Details} >
-                    {props.content}
-                 </div>
-             </span>
-        </div>);
+    const width = props.commit.w*props.width + 'px';
+    const color = props.commit.y ? (commit.y === 1 ? '#ff3c00' : '#ff5c50' ) : null;  
+    const classStyle = (props.commit.i[0] === 'c') ? CommitStyles : BranchStyles;
+    const newBranch = (props.commit.i[0] === 'c') ? null :
+                  <img draggable = {false}  src={newbranch} width={props.width} height={60}/>;
+    const inlineStyle = {borderBottomColor:color?color:'#ff7b00',width:width?width:'50px'};
+    return (
+        <div className= {classStyle.Commit} style={inlineStyle}>
+            <span onClick = {props.clicked}>
+                <div  className= {classStyle.Details} >
+                    {props.commit.content}
+                </div>
+            </span>
+            {newBranch}
+        </div> );
 } 
 export default commit;
