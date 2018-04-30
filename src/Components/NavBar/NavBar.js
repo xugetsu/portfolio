62bc916c1cc572a4d29b83ed5744004d38dabe78
Othmani Ali <othmanixali@gmail.com>
2018-05-01 00:07:45 +0100

    Imp RawLogo2 cmp in NavBar cmp

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import styles from './NavBar.css'
+import styles from './NavBar.css';
+import RawLogo from './MyLogoRaw2';
 const navBar = () => {
         return(
-            <nav className={styles.NavBar}>
-                <a href='/'>Home</a>
-                <a href='/'>About</a>
-                <a href='/'>Portfolio</a>
-            </nav>    
+            <div>
+                <div className={styles.RawLogoWrapper}> 
+                    <RawLogo className={styles.RawLogo}logoHeight={130}/>
+                </div>
+                <nav className={styles.NavBar}>
+                    <a href='/'>Home</a>
+                    <a href='/'>About</a>
+                    <a href='/'>Portfolio</a>
+                </nav>    
+            </div>
         );
     
 }
