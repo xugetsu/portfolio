81f2bbf593a4199e1b3f70419fbbf25f18842707
Othmani Ali <othmanixali@gmail.com>
2018-05-01 06:48:06 +0100

    M style Spinner cmp

--- src/Components/Spinner/Spinner.js
+++ src/Components/Spinner/Spinner.js
@@ -2,9 +2,9 @@ import React from 'react';
 import classes from './Spinner.css';
 
 const spinner = () => (
-    <div className={classes.Load}>
-    <div className={classes.Loader}></div>
-    <p>Loading...</p>
+    <div className={classes.Spinner}>
+        <div className={classes.Loader}></div>
+        <p>Loading...</p>
     </div>
 );
 
