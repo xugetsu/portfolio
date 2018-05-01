69662b3c0f3b847026636511fbc00ccd6a3a6fb9
Othmani Ali <othmanixali@gmail.com>
2018-05-01 06:05:04 +0100

    adding custom height in customScrollbars cmp

--- src/Components/UI/CustomScrollbars/CustomScrollbars.js
+++ src/Components/UI/CustomScrollbars/CustomScrollbars.js
@@ -4,8 +4,10 @@ import { Scrollbars } from 'react-custom-scrollbars';
 
 const customScrollbars = (props) => {
     const style = {cursor: 'pointer'};
+    // const scrollColor = props.scrollColor;
     return (
-        <Scrollbars style={{ width: '100%', height: '100%' }}
+        <Scrollbars style={{ width: '100%', height: props.height}}
+          //autoHeight
           //  renderTrackHorizontal={props => <div {...props} />}//className="track-horizontal"
           //  renderThumbHorizontal={(style,props) => <div {...props} style={{ ...style, backgroundColor: 'blue' }} />}//className="thumb-horizontal"
             renderTrackVertical= { (props) => <div {...props} className={styles.TrackVertical} style={style}/>}    
