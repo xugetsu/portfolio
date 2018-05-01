55af8bf33d76e80d8e849293782b60c09c87de3b
Othmani Ali <othmanixali@gmail.com>
2018-05-01 22:42:15 +0100

    Add logic to Spinner cmp for better user experience

--- src/Components/Spinner/Spinner.js
+++ src/Components/Spinner/Spinner.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import classes from './Spinner.css';
 
-const spinner = () => (
-    <div className={classes.Spinner}>
-        <div className={classes.Loader}></div>
-        <p>Loading...</p>
-    </div>
-);
+const spinner = (props) => {
+    
+    if (props.error) {
+        console.log(props.error);
+        // When the loader has errored
+        return <div>Error!</div>;
+    }else if(props.timedOut) {
+        // When the loader has taken longer than the timeout
+        return <div>Taking a long time...</div>;
+    }else if(props.pastDelay) {
+        // When the loader has taken longer than the delay
+        return (<div className={classes.Spinner}>
+                    <div className={classes.Loader}></div>
+                    <p>Loading...</p>
+                </div>);       
+    }else{
+        // When the loader has just started
+         return null;
+    }
+};
 
 export default spinner;
