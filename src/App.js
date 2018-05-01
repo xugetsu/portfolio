2644f792a61db795de9d128c7d86e4f990ee528a
Othmani Ali <othmanixali@gmail.com>
2018-05-01 06:11:55 +0100

    Imp CustomScrollbars in App cmp

--- src/App.js
+++ src/App.js
@@ -6,14 +6,17 @@ import NavBar from './Components/NavBar/NavBar';
 import Modules from './Components/modules/Modules';
 import GeneralInfo from './Components/GeneralInfo/GeneralInfo';
 import GitMyCareer from './Components/GitMyCareer/GitMyCareer';
+import CustomScrollbars from './Components/UI/CustomScrollbars/CustomScrollbars';
 class App extends Component {
   render() {
     return (
       <div className={styles.App}>
-        <NavBar />
-        <GeneralInfo />
-        <GitMyCareer />
-        <BannerLogo />
+         <CustomScrollbars height = {window.innerHeight }>
+            <NavBar />
+            <GeneralInfo />
+            <GitMyCareer />
+            <BannerLogo />
+        </CustomScrollbars>
       </div>
     );
   }
