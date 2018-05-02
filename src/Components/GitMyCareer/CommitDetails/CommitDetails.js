276a1c4eb64b791dab6cb05da028d8c38694bad7
Othmani Ali <othmanixali@gmail.com>
2018-05-02 06:41:29 +0100

    several Updates & bug fix for scrollbar in CommitDetails cmp

--- src/Components/GitMyCareer/CommitDetails/CommitDetails.js
+++ src/Components/GitMyCareer/CommitDetails/CommitDetails.js
@@ -7,18 +7,30 @@ import Spinner from '../../Spinner/Spinner';
 const commitDetails = (props) => {
     const Commit = Loadable({
         loader: () => import('./Commits/Commit' + props.hash),
+        //.catch( error => console.log('Handled Error :',error) ),
         loading: Spinner,
+        // Issue to fix : Unhandled Rejection when import failed...
+        // If we use catch with import as commented above, the error will no longer 
+        // transmited to Spinner's props. Which is unwanted behavior.
+        timeout: 10000, // 10 seconds
+        render(loaded, props){
+            let LoadedComponent = loaded.default;
+            return  (<CustomScrollbars scrollId={props.hash} width = {'100%'} height={'380px'} zIndex={2} >
+                        <LoadedComponent />
+                    </CustomScrollbars>);
+        }
     });
+    const date= props.date;
     return (
-      <div className={styles.CommitDetails} >
-         <CustomScrollbars height={'100%'} zIndex={2}>
-            <div className= {styles.Commit}>
-               <h2 className={styles.Message}> {props.title} <span>{props.date}</span></h2>
-               <Commit />
-            </div>
-         </CustomScrollbars>
-      </div>
-    );
+        <div className={styles.CommitDetails} >                
+            <h2 className={styles.Message}> {props.title} 
+                <span>
+                    {'On, '+date.slice(0,3)+'. '+date.slice(4,6)+' '+date.slice(7,10)+', '+date.slice(11)}
+                </span>
+            </h2>
+            <br/> <Commit />
+        </div>
+    );      
 }
 export default commitDetails;
 
