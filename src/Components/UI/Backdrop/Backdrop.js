217d6b7b4ea03bbfe89b4909e7359b7a55206155
Othmani Ali <othmanixali@gmail.com>
2018-05-03 06:13:01 +0100

    Create Backdrop cmp

---
+++ src/Components/UI/Backdrop/Backdrop.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import styles from './Backdrop.css';
+
+const backdrop = (props) => props.show ? <div   className = {styles.Backdrop} 
+                                                onClick = {props.clicked} ></div> : null
+
+export default backdrop;
