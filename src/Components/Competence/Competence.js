2bbdd80d1e3a8eee87219c72f2334c260e61fe28
Othmani Ali <othmanixali@gmail.com>
2018-05-03 17:55:15 +0100

    Slite changing in grid layout in  Competence cmp

--- src/Components/Competence/Competence.js
+++ src/Components/Competence/Competence.js
@@ -20,7 +20,7 @@ const  competence  = () => {
                     {i: 'b', x: 0, y: 0, w: 3, h: 4, static: true},
                     {i: 'g', x: 3, y: 0, w: 3, h: 4, static: true},
                     {i: 'r', x: 6, y: 0, w: 3, h: 4, static: true},
-                    {i: 'y', x: 2, y: 4, w: 5, h: 3, static: true},
+                    {i: 'y', x: 1, y: 4, w: 7, h: 3, static: true},
     ];
     return (
         <div className={styles.Competence}>
@@ -40,9 +40,9 @@ const  competence  = () => {
                 <div key="b" className={styles.Basic}>
                     <h2>Basic Web Developpment Languages</h2>
                     <ul> 
-                        <li><img src={HTML5} height={100} alt='HTML5'/> <p>HTML5</p></li>
-                        <li><img src={CSS3} height={100} alt='CSS3'/> <p>CSS3</p></li>
-                        <li><img src={JavaScript} height={100} alt='JavaScript'/> <p>JavaScript</p></li>
+                        <li><img src={HTML5} height={100} alt='HTML5'/></li>
+                        <li><img src={CSS3} height={100} alt='CSS3'/></li>
+                        <li><img src={JavaScript} height={100} alt='JavaScript'/></li>
                     </ul>
                 </div>               
                 <div key="g" className={styles.Advanced}>
