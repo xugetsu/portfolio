fb25db5fafbef086376b767e8da7b5246814f350
Othmani Ali <othmanixali@gmail.com>
2018-05-03 18:07:19 +0100

    Imp Projects, Projects1 & modal in Portfolio cmp

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,29 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import styles from './Portfolio.css';
-import game2048 from '../../Assets/Images/2048b.PNG';
-import pportfolio from '../../Assets/Images/portfolio.PNG';
-const portfolio = () => {
-    return (
-        <div className= {styles.Portfolio}>
-           <h1>My Portfolio</h1>
-           <div className= {styles.Projects}>
-                <div  className= {styles.Project}>
-                     <img className= {styles.Img} src={game2048} alt='game 2048 ultra'/>
-                     <div className= {styles.Window}><p>Game 2048 ultra!</p></div>
-                </div>
-                <div  className= {styles.Project}>
-                     <img className= {styles.Img} src={pportfolio} alt='project portfolio'/>
-                     <div className= {styles.Window}><p>My Website</p></div>
-                </div>
-                <div  className= {styles.Project}>
-                     <img className= {styles.Img} src={game2048} alt='game 2048 ultra'/>
-                     <div className= {styles.Window}><p>Game 2048 ultra!</p></div>
-                </div>
-
-           </div>
-        </div>
-    );
+import Modal from '../UI/Modal/Modal';
+import Projects from './Projects/Projects';
+import Project1 from './Projects/Project1/Project1';
+class Portfolio extends Component{
+    state = {
+        projectNumber: 1,
+        showModal: false
+    }
+    hideModalHandler = (e) => {
+        this.setState({showModal:false});
+    }
+    showModalHandler = (projectNumber) => {
+        this.setState({showModal:true, projectNumber:projectNumber});
+    }
+    render (){
+        return (
+            <div className= {styles.Portfolio}>
+                    <Modal show={this.state.showModal} exit = {this.hideModalHandler}> 
+                        <Project1 />
+                    </Modal>
+               <h1>My Portfolio</h1>
+               <Projects showProject = {(i) => this.showModalHandler(i)}/>
+            </div>
+        );
+    }    
 }
 
-export default portfolio;
+export default Portfolio;
 
