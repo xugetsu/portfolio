52f7295f20624b8205eea4cddcd73d6aaf2408d8
Othmani Ali <othmanixali@gmail.com>
2018-05-03 22:00:41 +0100

    R gif to gifGame2048

--- src/Components/Portfolio/Projects/Project1/GifGame2048.js
+++ src/Components/Portfolio/Projects/Project1/GifGame2048.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import game2048 from '../../../../Assets/Gifs/2048.gif';
 import Spinner from '../../../Spinner/Spinner';
-const gif = () => <img style={{maxWidth:'500px'}} src={game2048} alt='game 2048 ultra'/>;
+const gifGame2048 = () => <img style={{maxWidth:'500px'}} src={game2048} alt='game 2048 ultra'/>;
 
-export default gif;
+export default gifGame2048;
 
