9cc7ff1ff888c833646e8196b945e6108a6268e9
Othmani Ali <othmanixali@gmail.com>
2018-05-06 00:37:35 +0100

    M style GeneralInfo

--- src/Components/GeneralInfo/GeneralInfo.js
+++ src/Components/GeneralInfo/GeneralInfo.js
@@ -10,24 +10,26 @@ import MyLogoRaw from '../MyLogoRAW/MyLogoRaw';
 const generalInfo = () => {
     
     return (
-        <div className= {styles.GeneralInfo}>
-                <div className= {styles.Logo} >
-                        <MyLogoRaw colorFill1 = {'#23262b'} colorFill2 = {'#1f2227'} logoHeight = {900}/>
-                </div>   
-                <img  className= {styles.MyPhoto} alt='ali othmani' src={myPhoto} height={200}/>
-                <img  draggable = {false} alt='' className= {styles.Gear} src={gear} height={800}/>
-                <ul className= {styles.Info} >
-                    <li><b>{'"general-Info" : {'}</b></li>
-                    <li><span>" FULL NAME " :</span> " Ali Othmani "<b> ,</b></li>
-                    <li><span>" BORN " :</span> " [3, 9, 1992] "<b> ,</b></li>
-                    <li><span>" EMAIL " :</span> " othmanixali@gmail.com "<b>,</b></li>
-                    <li><span>" ADDRESS " :</span> " Sousse - TN "<b> ,</b></li>
-                    <li><span>" PHONE " :</span> " parseInt('175CC0A', 16) "</li>
-                    <li><b>{'}'}</b></li>
-                </ul>
-                <Col2 />
-                <AboutMe /> 
-
+        <div className= {styles.GeneralInfoWrapper}>
+            <div className= {styles.SkewMe}></div>
+            {/* <img  draggable = {false} alt='' className= {styles.Gear} src={gear} height={800}/> */}
+            <div className= {styles.GeneralInfo}>
+                    <div className= {styles.Logo} >
+                            <MyLogoRaw colorFill1 = {'#23262b'} colorFill2 = {'#1f2227'} logoHeight = {1200}/>
+                    </div>   
+                    <img  className= {styles.MyPhoto} alt='ali othmani' src={myPhoto} height={200}/>
+                    <ul className= {styles.Info} >
+                        <li><b>{'"general-Info" : {'}</b></li>
+                        <li><span>" FULL NAME " :</span> " Ali Othmani "<b> ,</b></li>
+                        <li><span>" BORN " :</span> " [3, 9, 1992] "<b> ,</b></li>
+                        <li><span>" EMAIL " :</span> " othmanixali@gmail.com "<b>,</b></li>
+                        <li><span>" ADDRESS " :</span> " Sousse - TN "<b> ,</b></li>
+                        <li><span>" PHONE " :</span> "24497162"</li>{/*parseInt('175CC0A', 16)*/}
+                        <li><b>{'}'}</b></li>
+                    </ul>
+                    <Col2 />
+                    <AboutMe /> 
+            </div>
         </div>
     );
 }
