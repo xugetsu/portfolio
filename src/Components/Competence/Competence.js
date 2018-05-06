97ed1a787bca5453e4432636e3255a36ac8113b4
Othmani Ali <othmanixali@gmail.com>
2018-05-06 01:50:58 +0100

    Removing GridLayout cmp and replace it with normal grid styling

--- src/Components/Competence/Competence.js
+++ src/Components/Competence/Competence.js
@@ -24,52 +24,46 @@ const  competence  = () => {
     ];
     return (
         <div className={styles.Competence}>
-        <GridLayout margin = {[0,0]} 
-            layout = {layout} 
-            cols = {9} 
-            rowHeight = {90} 
-            width = {window.innerWidth} >
-                <div key="r" className={styles.Basic}>
-                    <h2>Git and hosting services</h2>
-                    <ul> 
-                        <li><img src={git} height={60} alt='git'/> <p>GIT</p></li>
-                        <li><img src={gitlab} height={60} alt='gitlab'/> <p>gitlab</p></li>
-                        <li><img src={github} height={60} alt='github'/><p>github</p></li>  
-                    </ul>              
-                </div>
-                <div key="b" className={styles.Basic}>
-                    <h2>Basic Web Developpment Languages</h2>
-                    <ul> 
-                        <li><img src={HTML5} height={100} alt='HTML5'/></li>
-                        <li><img src={CSS3} height={100} alt='CSS3'/></li>
-                        <li><img src={JavaScript} height={100} alt='JavaScript'/></li>
-                    </ul>
-                </div>               
-                <div key="g" className={styles.Advanced}>
-                    <h2>Libraries Related to<br/> JavaScript</h2>
-                    <ul>
-                        <li><img src={react} height={60} alt='react'/> <p>React</p></li>
-                        <li><img src={redux} height={60} alt='redux'/> <p>Redux</p></li>
-                        <li><img src={webpack} height={60} alt='webpack'/> <p>WebPack</p></li>
-                        <li><img src={npm} height={30} alt='npm'/> <p>npm</p> </li>
-                    </ul>
-                </div>
-                <div key="y" className={styles.Dependencies}>
-                    <h2>Dependencies / Packages Related to React</h2>
-                    <ul>
-                        <li> <p>create-react-app</p></li>
-                        <li> <p>react-parallax</p></li>
-                        <li> <p>react-axios</p></li>
-                        <li> <p>react-css-variables</p></li>
-                        <li> <p>react-draggable</p></li>
-                        <li> <p>react-loadable</p></li>
-                        <li> <p>react-grid-layout</p></li>
-                        <li> <p>react-custom-scrollbars</p></li>
-                        <li> <p>react-router-dom</p></li>
-                        <li> <p>react-lazyload</p></li>
-                    </ul>
-                </div>
-        </GridLayout>    
+            <div key="r" className={styles.Git}>
+                <h2>Git and hosting services</h2>
+                <ul> 
+                    <li><img src={git} height={60} alt='git'/> <p>GIT</p></li>
+                    <li><img src={gitlab} height={60} alt='gitlab'/> <p>gitlab</p></li>
+                    <li><img src={github} height={60} alt='github'/><p>github</p></li>  
+                </ul>              
+            </div>
+            <div key="b" className={styles.Basic}>
+                <h2>Basic Web Developpment Languages</h2>
+                <ul> 
+                    <li><img src={HTML5} height={100} alt='HTML5'/></li>
+                    <li><img src={CSS3} height={100} alt='CSS3'/></li>
+                    <li><img src={JavaScript} height={100} alt='JavaScript'/></li>
+                </ul>
+            </div>               
+            <div key="g" className={styles.Advanced}>
+                <h2>Libraries Related to<br/> JavaScript</h2>
+                <ul>
+                    <li><img src={react} height={60} alt='react'/> <p>React</p></li>
+                    <li><img src={redux} height={60} alt='redux'/> <p>Redux</p></li>
+                    <li><img src={webpack} height={60} alt='webpack'/> <p>WebPack</p></li>
+                    <li><img src={npm} height={30} alt='npm'/> <p>npm</p> </li>
+                </ul>
+            </div>
+            <div key="y" className={styles.Dependencies}>
+                <h2>Dependencies / Packages Related to React</h2>
+                <ul>
+                    <li> <p>create-react-app</p></li>
+                    <li> <p>react-parallax</p></li>
+                    <li> <p>react-axios</p></li>
+                    <li> <p>react-css-variables</p></li>
+                    <li> <p>react-draggable</p></li>
+                    <li> <p>react-loadable</p></li>
+                    <li> <p>react-grid-layout</p></li>
+                    <li> <p>react-custom-scrollbars</p></li>
+                    <li> <p>react-router-dom</p></li>
+                    <li> <p>react-lazyload</p></li>
+                </ul>
+            </div>    
         </div>
         //         <li><img src={python} height={50} alt='python'/> <p>python</p></li>
         //   <li><img src={reactRouter} height={40} alt='reactRouter'/><p>react-router</p></li>
