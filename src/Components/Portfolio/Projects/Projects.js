7ceb515518f5da11fff47bb98b51c5af7738a2be
Othmani Ali <othmanixali@gmail.com>
2018-05-06 15:57:19 +0100

    U Projects cmp

--- src/Components/Portfolio/Projects/Projects.js
+++ src/Components/Portfolio/Projects/Projects.js
@@ -3,6 +3,8 @@ import styles from './Projects.css';
 import game2048 from '../../../Assets/Images/2048b.PNG';
 import pportfolio from '../../../Assets/Images/portfolio.PNG';
 import zoom from '../../../Assets/Icons/svgs/zoom.svg';
+import burger from '../../../Assets/Images/burgerbuilder.PNG';
+import career from '../../../Assets/Images/career.PNG';
 const projects = (props) => {
     return (
             <div className= {styles.Projects}>
@@ -18,21 +20,21 @@ const projects = (props) => {
                 </div>
                 <div  className= {styles.Project}>
                     <img className= {styles.Img} 
-                         src={pportfolio} 
-                         alt='project portfolio'/>
+                         src={burger} 
+                         alt='Burger Builder App'/>
                     <div className= {styles.Window}
                          onClick = {() => props.showProject(2)}>
-                         <p>My Website</p>
+                         <p>Burger Builder App</p>
                          <img  className= {styles.zoom} src={zoom} alt='zoom'/>
                     </div>
                 </div>
                 <div  className= {styles.Project}>
                     <img className= {styles.Img} 
-                         src={game2048} 
-                         alt='game 2048 ultra'/>
+                         src={career} 
+                         alt='My career repository'/>
                     <div className= {styles.Window}
                          onClick = {() => props.showProject(3)}>
-                         <p>Game 2048 ultra!</p>
+                         <p>My career repository App</p>
                          <img className= {styles.zoom} src={zoom} alt='zoom'/>
                     </div>
                 </div>
