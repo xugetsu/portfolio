5711a68248ab70d32f6476a65a42b9c4e96f9122
Othmani Ali <othmanixali@gmail.com>
2018-05-07 22:49:59 +0100

    Form Validation and sending data to the server

--- src/Components/Contact/ContactData/ContactData.js
+++ src/Components/Contact/ContactData/ContactData.js
@@ -2,16 +2,22 @@ import React, {Component} from 'react';
 import styles from './ContactData.css';
 import Input from '../../UI/Input/Input';
 import CustomScrollbars from '../../UI/CustomScrollbars/CustomScrollbars';
+import axios from '../../../axios-orders';
 class ContactData extends Component{
     state = {
-        orderForm: {                   // the price need to be calculated in the server !!
+        messageForm: {
             name: {
                 elementType: 'input',
                 elementConfig: {
                     type: 'text',
                     placeholder: 'Name'
                 },
-                value: ''
+                value: '',
+                validation:{
+                    required:true
+                },
+                valid:false,
+                touched:false
             },
             email: {
                 elementType: 'input',
@@ -19,7 +25,12 @@ class ContactData extends Component{
                     type: 'email',
                     placeholder: 'E-Mail'
                 },
-                value: ''
+                value: '',
+                validation:{
+                    required:true
+                },
+                valid:false,
+                touched:false
             },
             subject: {
                 elementType: 'input',
@@ -27,7 +38,12 @@ class ContactData extends Component{
                     type: 'email',
                     placeholder: 'Subject'
                 },
-                value: ''
+                value: '',
+                validation:{
+                    required:true
+                },
+                valid:false,
+                touched:false
             },
             message: {
                 elementType: 'textarea',
@@ -35,37 +51,74 @@ class ContactData extends Component{
                     type: 'textarea',
                     placeholder: 'Message'
                 },
-                value: ''
+                value: '',
+                validation:{
+                    required:true
+                },
+                valid:false,
+                touched:false
             },
         },
-        purchaseLoading:false,
+        formTouched:false,
+        sending: false,
+        sent:false
+    }
+
+    checkValidity = (value, rules) =>{
+        let isValid = true;
+        if(rules && rules.required){ // input required to be filled to be valid
+            isValid = value.trim() !== ''; // inputs with just spaces are invalid too (this is why we use trim)
+        }
+        return isValid;
     }
 
     onChangeHandler = (event, inputIdentifier) => {
-        const updatedOrderForm = {
-            ...this.state.orderForm
-        };
-        updatedOrderForm[inputIdentifier].value= event.target.value;
-        this.setState({orderForm:updatedOrderForm});
+        const updatedMessageForm = { ...this.state.messageForm };
+        const rules = updatedMessageForm[inputIdentifier].validation;
+        updatedMessageForm[inputIdentifier].value = event.target.value;
+        updatedMessageForm[inputIdentifier].valid = this.checkValidity(event.target.value, rules);
+        updatedMessageForm[inputIdentifier].touched = true;
+
+        this.setState({messageForm: updatedMessageForm});
     }
-    
+
+    SendMessageHandler = (event) =>{
+        event.preventDefault();
+        this.setState({sending: true, formTouched: true});
+        const formData = {};
+        for( let info in this.state.messageForm){
+            formData[info] = this.state.messageForm[info].value;
+        }
+        const message = {
+            date: new Date().toDateString(),
+            formData: formData,
+        };       
+        axios.post('/messages.json', message)
+            .then ( response => {
+                    this.setState({sent: true, sending: false});
+                })
+            .catch( error => {this.setState({sending: false});});
+    }
+
     render(){
-        const orderForm = this.state.orderForm;
-        const inputs = Object.keys(orderForm)
+        const messageForm = this.state.messageForm;
+        const inputs = Object.keys(messageForm)
             .map( inputSetupKey => {
-                    const inputSetup = orderForm[inputSetupKey];
-                    return <Input   key={inputSetupKey}
-                                    elementType={inputSetup.elementType} 
-                                    elementConfig={inputSetup.elementConfig}
-                                    value={inputSetup.value}
-                                    changed={(e) => this.onChangeHandler(e, inputSetupKey)}/>
-                        });
+            const inputSetup = messageForm[inputSetupKey];
+            const inputIsInvalid = Boolean( !inputSetup.valid && (inputSetup.touched || this.state.formTouched) ); 
+            return <Input   key={inputSetupKey}
+                            elementType={inputSetup.elementType} 
+                            elementConfig={inputSetup.elementConfig}
+                            value={inputSetup.value}
+                            invalid= {inputIsInvalid}
+                            changed={(e) => this.onChangeHandler(e, inputSetupKey)}/>
+                });
 
         const iconSize = 80;
         return (
             <div id='Contact' className= {styles.ContactData}>
                 <h1>Send Me A Message</h1>
-                    <form onSubmit = {this.SendHandler}>
+                    <form onSubmit = {this.SendMessageHandler}>
                         <CustomScrollbars scrollId={3} height='360px' zIndex={10} width={'100%'}>
                             {inputs}
                         </CustomScrollbars>
--- src/Components/UI/Input/Input.js
+++ src/Components/UI/Input/Input.js
@@ -2,46 +2,49 @@ import React from 'react';
 import classes from './Input.css'
 
 const input = (props) => {
-
-        let inputElement = null;
-        switch (props.elementType) {
-            case 'input':    
-                 inputElement = <input className={classes.InputElement} 
-                                       {...props.elementConfig}
-                                       value={props.value}
-                                       onChange={props.changed}/>;     
-                break;
-            case 'textarea':  
-                inputElement = <textarea className={classes.InputElement} 
-                                          cols="50" rows='20'
-                                         {...props.elementConfig}
-                                         value={props.value}
-                                         onChange={props.changed}/>; 
-                break;
-            case 'select':  
-                inputElement =  <select className={classes.InputElement} 
-                                        value={props.value}
-                                        onChange={props.changed}>
-                                        {props.elementConfig.options.map( option =>(
-                                            <option key={option.value}
-                                                    value={option.value}>
-                                                    {option.displayValue}
-                                            </option>
-                                        ))}
-                                </select>; 
-                break;
-            default:          
-                inputElement = <input   className={classes.InputElement} 
+    const inputClasses = [classes.InputElement];
+    if(props.invalid){ 
+        inputClasses.push(classes.Invalid);
+    }
+    let inputElement = null;
+    switch (props.elementType) {
+        case 'input':    
+            inputElement = <input className={inputClasses.join(" ")} 
+                                {...props.elementConfig}
+                                value={props.value}
+                                onChange={props.changed}/>;     
+            break;
+        case 'textarea':  
+            inputElement = <textarea className={inputClasses.join(" ")} 
+                                        cols="50" rows='20'
                                         {...props.elementConfig}
-                                        value={props.value}/>;   
-                 break;
-        }
-        return(
-             <div className={classes.Input}>
-                <label className={classes.Label} >{props.label}</label>
-                {inputElement}
-             </div>
-        );
+                                        value={props.value}
+                                        onChange={props.changed}/>; 
+            break;
+        case 'select':  
+            inputElement =  <select className={inputClasses.join(" ")} 
+                                    value={props.value}
+                                    onChange={props.changed}>
+                                    {props.elementConfig.options.map( option =>(
+                                        <option key={option.value}
+                                                value={option.value}>
+                                                {option.displayValue}
+                                        </option>
+                                    ))}
+                            </select>; 
+            break;
+        default:          
+            inputElement = <input   className={inputClasses.join(" ")} 
+                                    {...props.elementConfig}
+                                    value={props.value}/>;   
+                break;
+    }
+    return(
+        <div className={classes.Input}>
+        <label className={classes.Label} >{props.label}</label>
+        {inputElement}
+        </div>
+    );
 }
 
 export default input;
