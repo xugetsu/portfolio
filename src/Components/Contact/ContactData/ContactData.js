4ad3e48b44e8911a35331172c8d9178d438d90fb
Othmani Ali <othmanixali@gmail.com>
2018-05-07 23:33:06 +0100

    U ContactData : bug fix + UI improvement

--- src/Components/Contact/ContactData/ContactData.js
+++ src/Components/Contact/ContactData/ContactData.js
@@ -3,6 +3,7 @@ import styles from './ContactData.css';
 import Input from '../../UI/Input/Input';
 import CustomScrollbars from '../../UI/CustomScrollbars/CustomScrollbars';
 import axios from '../../../axios-orders';
+import Spinner from '../../Spinner/Spinner';
 class ContactData extends Component{
     state = {
         messageForm: {
@@ -35,7 +36,7 @@ class ContactData extends Component{
             subject: {
                 elementType: 'input',
                 elementConfig: {
-                    type: 'email',
+                    type: 'text',
                     placeholder: 'Subject'
                 },
                 value: '',
@@ -72,6 +73,16 @@ class ContactData extends Component{
         return isValid;
     }
 
+    checkFormValidity = () => {
+        for( let inputElementKey in this.state.messageForm){
+            const inputElement = this.state.messageForm[inputElementKey]
+            if(! inputElement.valid){ 
+                return false;
+            }
+        }
+        return true;
+    }
+
     onChangeHandler = (event, inputIdentifier) => {
         const updatedMessageForm = { ...this.state.messageForm };
         const rules = updatedMessageForm[inputIdentifier].validation;
@@ -84,46 +95,63 @@ class ContactData extends Component{
 
     SendMessageHandler = (event) =>{
         event.preventDefault();
-        this.setState({sending: true, formTouched: true});
-        const formData = {};
-        for( let info in this.state.messageForm){
-            formData[info] = this.state.messageForm[info].value;
+        if(!this.checkFormValidity()){
+            this.setState({formTouched: true});
+            return null; // the form is invalid don't send anything !!
+        }else{
+            this.setState({sending: true});
+            const formData = {};
+            for( let info in this.state.messageForm){
+                formData[info] = this.state.messageForm[info].value;
+            }
+            const message = {
+                date: new Date().toDateString(),
+                formData: formData,
+            };       
+            axios.post('/messages.json', message)
+                .then ( response => {
+                        this.setState({sent: true, sending: false});
+                    })
+                .catch( error => {this.setState({sending: false});});
         }
-        const message = {
-            date: new Date().toDateString(),
-            formData: formData,
-        };       
-        axios.post('/messages.json', message)
-            .then ( response => {
-                    this.setState({sent: true, sending: false});
-                })
-            .catch( error => {this.setState({sending: false});});
     }
 
     render(){
-        const messageForm = this.state.messageForm;
-        const inputs = Object.keys(messageForm)
-            .map( inputSetupKey => {
-            const inputSetup = messageForm[inputSetupKey];
-            const inputIsInvalid = Boolean( !inputSetup.valid && (inputSetup.touched || this.state.formTouched) ); 
-            return <Input   key={inputSetupKey}
-                            elementType={inputSetup.elementType} 
-                            elementConfig={inputSetup.elementConfig}
-                            value={inputSetup.value}
-                            invalid= {inputIsInvalid}
-                            changed={(e) => this.onChangeHandler(e, inputSetupKey)}/>
-                });
+        let contentToDisplay = null;
+        if(this.state.sending){
+            contentToDisplay = <Spinner pastDelay send/>;
+        }else if(this.state.sent){
+            contentToDisplay = (<div>
+                                    <h1>{'Thank you!'}</h1>
+                                    <h2> {'Your message has been sent successfully :)'}</h2>
+                                </div>);
+        }else{
+            const messageForm = this.state.messageForm;
+            const inputs = Object.keys(messageForm)
+                .map( inputSetupKey => {
+                const inputSetup = messageForm[inputSetupKey];
+                const inputIsInvalid = Boolean( !inputSetup.valid && (inputSetup.touched || this.state.formTouched) ); 
+                return <Input   key={inputSetupKey}
+                                elementType={inputSetup.elementType} 
+                                elementConfig={inputSetup.elementConfig}
+                                value={inputSetup.value}
+                                invalid= {inputIsInvalid}
+                                changed={(e) => this.onChangeHandler(e, inputSetupKey)}/>
+                    });
+            contentToDisplay = <form onSubmit = {this.SendMessageHandler}>
+                                    <h1>Send Me A Message</h1>
+                                    <CustomScrollbars scrollId={3} height='360px' zIndex={10} width={'100%'}>
+                                        {inputs}
+                                    </CustomScrollbars>
+                                    <button className={styles.Button}>Send</button>
+                                </form>;
+        }
 
-        const iconSize = 80;
+
+       
         return (
             <div id='Contact' className= {styles.ContactData}>
-                <h1>Send Me A Message</h1>
-                    <form onSubmit = {this.SendMessageHandler}>
-                        <CustomScrollbars scrollId={3} height='360px' zIndex={10} width={'100%'}>
-                            {inputs}
-                        </CustomScrollbars>
-                        <button className={styles.Button}>Send</button>
-                    </form>
+                {contentToDisplay}
             </div>
         );
     }    
