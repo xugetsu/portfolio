7c5882413bd42b66bb00bbcf677fb752cb631563
Othmani Ali <othmanixali@gmail.com>
2018-05-08 03:00:15 +0100

    Create SideDrawer cmp

---
+++ src/Components/UI/SideDrawer/SideDrawer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import A from '../../../hoc/Auxx';
+
+const sideDrawer = (props) => {
+    const SideDrawerStyles = [styles.SideDrawer, (props.open? styles.Open : styles.Close)].join(' ');
+    return (
+        <A>
+            {/* <Backdrop show={props.open} clicked={props.closeSideDrawer}/> */}
+            <div  className={SideDrawerStyles}>
+                <nav>
+                </nav>
+            </div>
+        </A>
+    );
+};
+
+export default sideDrawer;
