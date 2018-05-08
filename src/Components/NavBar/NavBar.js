05580cc0faea191c689e1c064cd813a8f7fbec97
Othmani Ali <othmanixali@gmail.com>
2018-05-08 01:33:44 +0100

    shrinking navBar with some modifications

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -2,13 +2,19 @@ import React from 'react';
 import styles from './NavBar.css';
 import MyLogo from '../MyLogoRAW/MyLogo';
 const navBar = (props) => {
+        const logoWrapperStyles = [styles.LogoWrapper];
+        const headerStyles = [''];
+        if(props.shrink){
+            logoWrapperStyles.push(styles.ShrinkLogo);
+            headerStyles.push(styles.Shrink);
+        }
         return(
-            <div>
-                <div className={styles.RawLogoWrapper} onClick={() => props.navigateTo(0)}> 
-                    <span className={styles.RawLogo} ><MyLogo  height = {60} width={100} id={2}
-                                openTagStopColor1='#2989d8'  openTagStopColor2='#2bade7'
-                                closeTagStopColor1='#00bcf2' closeTagStopColor2='#2bade7'
-                                slashStopColor1='#ebebeb'    slashStopColor2='#888888' />
+            <header className ={headerStyles.join(' ')}>
+                <div className={logoWrapperStyles.join(' ')} onClick={() => props.navigateTo(0)} >
+                    <span className={styles.RawLogo} > <MyLogo  height = {40} width={50} id={2}
+                            openTagStopColor1='#2989d8'  openTagStopColor2='#2bade7'
+                            closeTagStopColor1='#00bcf2' closeTagStopColor2='#2bade7'
+                            slashStopColor1='#ebebeb'    slashStopColor2='#888888' />
                     </span>
                 </div>
                 <nav className={styles.NavBar}>
@@ -18,7 +24,7 @@ const navBar = (props) => {
                     <button onClick={() => props.navigateTo(3)}>Portfolio</button>
                     <button onClick={() => props.navigateTo(4)}>Contact</button>
                 </nav>    
-            </div>
+            </header>
         );
     
 }
