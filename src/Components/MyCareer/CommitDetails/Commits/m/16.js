413136eacea6aa926723f92a5de79829ee1ef0b2
Othmani Ali <othmanixali@gmail.com>
2018-05-09 23:00:48 +0100

    Rename & create new commit files

---
+++ src/Components/MyCareer/CommitDetails/Commits/m/16.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const commit = () => {
+    return (
+        <article>
+            <p><br /> <br />More details coming soon.</p>
+        </article>
+    );
+}
+export default commit;
