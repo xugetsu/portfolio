fc0feb41e9ac9543bffe5116c9306ab41de8d577
Othmani Ali <othmanixali@gmail.com>
2018-05-09 12:33:56 +0100

    Updates in project1 cmp

--- src/Components/Portfolio/Projects/Project1/Project1.js
+++ src/Components/Portfolio/Projects/Project1/Project1.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Project1.css';
 import Loadable from 'react-loadable';
 import Spinner from '../../../Spinner/Spinner';
-
+import redirect from '../../../../Assets/Icons/svgs/externalink.svg';
 const project1 = () => {
     const Gif = Loadable({
         loader: () => import('./GifGame2048'),
@@ -16,9 +16,13 @@ const project1 = () => {
                 <Gif />
             </div>
             <article>
+                <div className= {styles.TryIt}>
+                <a  rel='noopener noreferrer' target='_blank' href='https://my-2048-game-with-react.firebaseapp.com/'><img  src={redirect} /></a>
+                    <span>try it!</span>
+                </div>
                 <h2>2048 Game Ultra</h2>
                 <p>This project was developed to put the knowledge and skills I've learned into practice
-                <br/>Inspired by the <a target="_blank" rel="noopener noreferrer" href='http://2048game.com/'>original game</a>, I've developped my game logic from scratch using the JavaScript library React.js</p> 
+                <br/>Inspired by the <a target="_blank" rel="noopener noreferrer" href='http://2048game.com/'>original game</a>, I've developped my own game logic from scratch using the JavaScript library React.js</p> 
                 <p><b>Development State : </b>Under construction</p>
             </article>
         </div>
