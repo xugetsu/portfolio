2d7e77ef8079cc3b55a6851ac9a158a46346a904
Othmani Ali <othmanixali@gmail.com>
2018-05-09 12:34:29 +0100

    create gifburger cmp

---
+++ src/Components/Portfolio/Projects/Project2/GifBurger.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import burger from '../../../../Assets/Images/burgerbuilder.PNG';
+const gifburger = () => <img style={{maxWidth:'500px'}} src={burger} alt='burger builder app'/>;
+
+export default gifburger;
+
