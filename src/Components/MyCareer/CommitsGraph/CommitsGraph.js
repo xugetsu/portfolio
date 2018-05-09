c45393dd14e8052a4ba596ad84a33d5570afe16c
Othmani Ali <othmanixali@gmail.com>
2018-05-09 07:38:33 +0100

    add logic to CommitsGraph cmp

--- src/Components/MyCareer/CommitsGraph/CommitsGraph.js
+++ src/Components/MyCareer/CommitsGraph/CommitsGraph.js
@@ -11,26 +11,40 @@ const commitsGraph = (props) => {
     console.log(layout);
 
     const commita1 = layout[0];
+    const commita3 = layout[2];
     const commitInfoa1 = {hash:commita1.i,content:commita1.content};
+    const commitInfoa3 = {hash:commita3.i,content:commita3.content};
     const commitElementa1  = <CommitElement commit = {commita1} 
                                             clicked = {() => props.loadCommitLog(commitInfoa1)}
                                             width = { commita1.w * gridWidth + 'px'}
                                             Height = {rowHeight}
                                             active = {commita1.i === props.currentHash}
                                             elementForm = {commita1.commitForm}/>;
+    const commitElementa3  = <CommitElement commit = {commita3} 
+                                            clicked = {() => props.loadCommitLog(commitInfoa1)}
+                                            width = { commita3.w * gridWidth + 'px'}
+                                            Height = {rowHeight}
+                                            active = {commita3.i === props.currentHash}
+                                            elementForm = {commita3.commitForm}/>;
 
     const commits = layout.map( commit => {
+        let elementWidth = commit.w * gridWidth + 'px';
+        if( commit.i === 'a2'  ){
+             elementWidth = 2* gridWidth + 'px' ;
+        }
         const commitInfo = {hash:commit.i,content:commit.content};
         const commita1 = commit.i === 'm7' ? commitElementa1 : null;    
-        const commitx  = commit.i === 'a1' ? null : <CommitElement  commit = {commit} 
+        const commita3 = commit.i === 'm10' ? commitElementa3 : null; 
+        const commitx  = (commit.i === 'a1' || commit.i === 'a3' )? null : <CommitElement  commit = {commit} 
                                                         clicked = {() => props.loadCommitLog(commitInfo)}
-                                                        width = { commit.w * gridWidth + 'px'}
+                                                        width = {elementWidth}
                                                         Height = {rowHeight}
                                                         active = {commit.i === props.currentHash}
                                                         elementForm = {commit.commitForm}
                                                         />
         return  <div key={commit.i} style={{position:'relative'}}>
                     <div style={{position:'absolute',bottom:'-5px'}}>{commita1}</div>
+                    <div style={{position:'absolute',bottom:'-8px'}}>{commita3}</div>
                     {commitx}
                 </div>;
      });
