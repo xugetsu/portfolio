53bc574faa30b27ea2a592afd3b372611b7f34d0
Othmani Ali <othmanixali@gmail.com>
2018-05-09 12:35:16 +0100

    lazyLoading projects in Portfolio cmp

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
 import styles from './Portfolio.css';
+import Loadable from 'react-loadable';
 import Modal from '../UI/Modal/Modal';
+import Spinner from '../Spinner/Spinner';
 import Projects from './Projects/Projects';
-import Project1 from './Projects/Project1/Project1';
 class Portfolio extends Component{
     state = {
         projectNumber: 1,
@@ -14,14 +15,22 @@ class Portfolio extends Component{
     showModalHandler = (projectNumber) => {
         this.setState({showModal:true, projectNumber:projectNumber});
     }
+    
     render (){
+        const n = this.state.projectNumber;
+        const Project = Loadable({
+            loader: () => import('./Projects/Project'+n+'/Project'+n),
+            loading: Spinner,
+            delay: 200,
+            timeout: 10000, // 10 seconds
+        });
         return (
             <div id='Portfolio' className= {styles.Portfolio}>
-                    <Modal show={this.state.showModal} exit = {this.hideModalHandler}> 
-                        <Project1 />
-                    </Modal>
-               <h1>My Portfolio</h1>
-               <Projects showProject = {(i) => this.showModalHandler(i)}/>
+                <Modal show={this.state.showModal} exit = {this.hideModalHandler}> 
+                   <Project />
+                </Modal>
+                <h1>My Portfolio</h1>
+                <Projects showProject = {(i) => this.showModalHandler(i)}/>
             </div>
         );
     }    
