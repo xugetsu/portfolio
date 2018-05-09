d95d72e92fff7922c19e64ada27aa871b98490b3
Othmani Ali <othmanixali@gmail.com>
2018-05-09 05:17:29 +0100

    M commitGraph cmp

--- src/Components/MyCareer/CommitsGraph/CommitsGraph.js
+++ src/Components/MyCareer/CommitsGraph/CommitsGraph.js
@@ -3,25 +3,43 @@ import styles from './CommitsGraph.css';
 import GridLayout from 'react-grid-layout';
 import Draggable from 'react-draggable';
 import CommitElement from './CommitElement/CommitElement';
-
+import Branch from './CommitElement/branch';
 const commitsGraph = (props) => {
     const gridWidth = 50; // grid Width
     const rowHeight = 55; // grid Height you can't change the height until you fix branch svg's relation between its height and width
     const layout = props.layout;
+    console.log(layout);
+
+    const commita1 = layout[0];
+    const commitInfoa1 = {hash:commita1.i,content:commita1.content};
+    const commitElementa1  = <CommitElement commit = {commita1} 
+                                            clicked = {() => props.loadCommitLog(commitInfoa1)}
+                                            width = { commita1.w * gridWidth + 'px'}
+                                            Height = {rowHeight}
+                                            active = {commita1.i === props.currentHash}
+                                            elementForm = {commita1.commitForm}/>;
+
     const commits = layout.map( commit => {
-        return  <div key={commit.i}>
-                    <CommitElement commit = {commit} 
-                            clicked = {() => props.loadCommitLog(commit.hash)}
-                            width = {gridWidth}
-                            Height = {rowHeight}
-                            active = {commit.hash === props.currentHash}/>
+        const commitInfo = {hash:commit.i,content:commit.content};
+        const commita1 = commit.i === 'm7' ? commitElementa1 : null;    
+        const commitx  = commit.i === 'a1' ? null : <CommitElement  commit = {commit} 
+                                                        clicked = {() => props.loadCommitLog(commitInfo)}
+                                                        width = { commit.w * gridWidth + 'px'}
+                                                        Height = {rowHeight}
+                                                        active = {commit.i === props.currentHash}
+                                                        elementForm = {commit.commitForm}
+                                                        />
+        return  <div key={commit.i} style={{position:'relative'}}>
+                    <div style={{position:'absolute',bottom:'-5px'}}>{commita1}</div>
+                    {commitx}
                 </div>;
-    });
+     });
+
     return (
         <div className={styles.CommitsGraph} >
             <Draggable  handle=".handle"
                         defaultPosition={{x: -100, y: 0 }}
-                        bounds={{bottom: 0, left: -100, right: 0, top: -100}}
+                        bounds={{bottom: 0, left: -400, right: 0, top: 0}}
                         position={null}
                         onStart={this.handleStart} 
                         onDrag={this.handleDrag} 
