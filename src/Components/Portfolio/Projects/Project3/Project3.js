98b2ea864823b391b8afdf63cb43b07d2f32a5a7
Othmani Ali <othmanixali@gmail.com>
2018-05-10 00:02:21 +0100

    Create Project3 cmp

---
+++ src/Components/Portfolio/Projects/Project3/Project3.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styles from './Project3.css';
+import Loadable from 'react-loadable';
+import Spinner from '../../../Spinner/Spinner';
+
+const project1 = () => {
+    const Gif = Loadable({
+        loader: () => import('./GifGame2048'),
+        loading: () => Spinner,
+        delay: 10,
+        });
+
+    return (
+        <div className= {styles.Project1}>
+              <div style={{height:'100%',width:'100%'}}>
+                <Gif />
+            </div>
+            <article>
+                <h2>My Creer Repository</h2>
+                <p>A creative way to represent my career progress
+                <br/><br/>
+                I just started developping this project (started in 1 May 2018) 
+                 and it will have more awsome updates very soon. Stay tuned ;) ..
+                 <br/><br/><b>Development State : </b>Under construction<br/></p>
+            </article>
+        </div>
+    );
+}
+
+export default project1;
+
---
+++ src/Components/Portfolio/Projects/Project3/careerImg.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import career from '../../../../Assets/Images/career.PNG';
+const gifGame2048 = () => <img style={{maxWidth:'500px'}} src={career} alt='game 2048 ultra'/>;
+
+export default gifGame2048;
+
