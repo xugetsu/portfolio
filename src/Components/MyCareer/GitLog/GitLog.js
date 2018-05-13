46151ec5fc19afca38210f50fd7b197733816e04
Othmani Ali <othmanixali@gmail.com>
2018-05-13 01:33:20 +0100

    Uodates in GitLog cmp

--- src/Components/MyCareer/GitLog/GitLog.js
+++ src/Components/MyCareer/GitLog/GitLog.js
@@ -1,27 +1,38 @@
 import React from 'react';
 import styles from './GitLog.css';
-import { Scrollbars } from 'react-custom-scrollbars';
+import CustomScrollbars from '../../UI/CustomScrollbars/CustomScrollbars';
 
 const gitLog = (props) => {
     let log = null;
-   
+
     if(props.commitsLog){
     const c = [...props.commitsLog].reverse();
     log = c.map(
-        commit => <div onClick={() => props.clicked(commit.rank)} 
-                       key={commit.rank} 
-                       style={(props.currentrank === commit.rank ? {color: '#9acd32'}: {})}
-                       className={styles.Commit}>
-                       {commit.content[0]+' - '+commit.content[1]}
-                  </div>
+        commit => {
+            const color = props.currentrank === commit.rank ? '#fff': 'inherit';
+            let backgroundColor = '#006ab142';
+            if(commit.hash[0] === 'm'){ backgroundColor =  '#fbff0042'}
+            if(commit.hash[0] === 'w'){ backgroundColor =  '#ff3c0042'}
+            if(props.currentrank === commit.rank ){ 
+                if(commit.hash[0] === 'm'){ backgroundColor =  '#fea511'}
+                else if(commit.hash[0] === 'w'){ backgroundColor =  '#ff3c00'}
+                else { backgroundColor =  '#0077ff'}
+            }
+            return <div onClick={() => props.clicked(commit.rank)} 
+                        key={commit.rank} 
+                        style={{color: color, backgroundColor: backgroundColor}}
+                        className={styles.Commit}>
+                        {commit.content[1]+' • '+commit.content[0]}
+                    </div>
+                }
     )};
     
     return (
         <div className= {styles.GitLog}>
          <h4>$ Career log --all --oneline </h4>
-            <Scrollbars style={{ width: '45vw', height: '280px'}}>
+            <CustomScrollbars  style1 width= '100%'  height= '280px' zindex={10}>
                 {log}
-           </Scrollbars>
+           </CustomScrollbars>
         </div>
     );
 }
