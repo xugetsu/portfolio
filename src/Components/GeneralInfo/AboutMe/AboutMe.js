758fc61d8d2b8e743dc354ce1404ad97ed4d1a1a
Othmani Ali <othmanixali@gmail.com>
2018-05-14 18:56:13 +0100

    typo fix

--- src/Components/GeneralInfo/AboutMe/AboutMe.js
+++ src/Components/GeneralInfo/AboutMe/AboutMe.js
@@ -11,7 +11,7 @@ const aboutMe = () => {
                 <li><span><LogoFragment2 h={300}/></span>I'm a React Js web developer and a mechatronic engineer.</li>
                 <li><span><LogoFragment2 h={300}/></span>I studied at the National School of Engineering of Sousse as a mechatronic engineer and, after graduation, I started learning about web development.</li>
                 <li><span><LogoFragment2 h={300}/></span>I was, and still am, very passionate about web development. This passion lead to my firm decision to keep going in this career path believing that, someday, I can benefit from both of my specialities together.</li>
-                <li><span><LogoFragment2 h={300}/></span>The first step toward achieving my goal is to have a <strong>permanent job</strong> as a <b>Full-Stack React Web Developer</b>.</li>
+                <li><span><LogoFragment2 h={300}/></span>The first step toward achieving my goal is to have a <strong>permanent job</strong> as a <b>Full-Stack Web Developer</b>.</li>
                 <li><span><LogoFragment2 h={300}/></span>This site will mainly show you details related to my experience as a web developer. If you are interrested to know further information about me, please visit my <a target="_blank" rel="noopener noreferrer" href='https://www.linkedin.com/in/ali-othmani-11873707/'>linkedin</a> profile.</li>
             </ul>
         </div>
