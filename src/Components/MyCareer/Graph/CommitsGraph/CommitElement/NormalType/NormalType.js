7509cc5ac079225d0de5f19da911a8eb4e8e9ba6
Othmani Ali <othmanixali@gmail.com>
2018-05-14 19:06:52 +0100

    U NormalType cmp

--- src/Components/MyCareer/Graph/CommitsGraph/CommitElement/NormalType/NormalType.js
+++ src/Components/MyCareer/Graph/CommitsGraph/CommitElement/NormalType/NormalType.js
@@ -2,14 +2,24 @@ import React from 'react';
 import styles from './NormalType.css';
 import Button from '../Button/Button';
 
-const normalType = (props) =>{
-        const _Button = !props.Btn ? null: <Button className = {styles.NormalButton}
-                                                content = {props.content} 
-                                                active  = {props.active}
-                                                clicked = {props.clicked} /> 
+const normalType = (props) => {
+        let button = null;
+        if(props.Btn){
+            let position = {left:'50%'};
+            switch (props.Btn){
+                case 'start':  position = {left:'0px'};break;
+                case 'end':    position = {right:'0px'};break;
+                default:break;
+            }
+            button = <Button  className = {styles.NormalButton}
+                              position = {position}
+                              content = {props.content} 
+                              active  = {props.active}
+                              clicked = {props.clicked} /> ;
+        }
     return (
-        <div key={props.key} className= {styles.NormalType} style={props.inlineStyle}>
-                {_Button}
+        <div className = {styles.NormalType} style= {props.inlineStyle}>
+                {button}
         </div>);
 } 
 export default normalType;
