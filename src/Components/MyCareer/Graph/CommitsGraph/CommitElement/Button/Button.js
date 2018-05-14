5f72064c042529e6e10535079129d83d256f5b48
Othmani Ali <othmanixali@gmail.com>
2018-05-14 19:05:57 +0100

    U Button cmp

--- src/Components/MyCareer/Graph/CommitsGraph/CommitElement/Button/Button.js
+++ src/Components/MyCareer/Graph/CommitsGraph/CommitElement/Button/Button.js
@@ -2,13 +2,13 @@ import React from 'react';
 import styles from './Button.css';
 
 const button = (props) => {
+    const btnStyles = props.active ? styles.Active : styles.Button;
+
     return (
-        <div className = {props.className}>
-            <span  className = {styles.Button}
-                onClick = {props.clicked} 
-                style = {{background:props.active ? '#0099ff':'#2b2b2b',
-                            transform:props.active ? 'scale(1.2)':''}}>
-                <div  className= {styles.Details} >
+        <div className = {props.className} style={props.position}>
+            <span  className = {btnStyles}
+                onClick = {props.clicked}>
+                <div  className= {styles.Details}>
                     {props.content}
                 </div>
             </span>
