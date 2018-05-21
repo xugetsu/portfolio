1d5095948f4b52fa65252d052272adb5e4555292
Othmani Ali <othmanixali@gmail.com>
2018-05-21 17:05:05 +0100

    U Competence cmp

--- src/Components/Competence/Competence.js
+++ src/Components/Competence/Competence.js
@@ -12,7 +12,9 @@ import github from '../../Assets/Icons/svgs//github.svg';
 import JavaScript from '../../Assets/Icons/svgs/javaScript.svg';
 import CSS3 from '../../Assets/Icons/svgs/css3.svg';
 import HTML5 from '../../Assets/Icons/svgs/html5.svg';
-// import python from '../../Assets/Icons/svgs/python.svg';
+import python from '../../Assets/Icons/svgs/python.svg';
+import java from '../../Assets/Icons/svgs/java.svg';
+import wolfram from '../../Assets/Icons/pngs/wolfram.png';
 
 const  competence  = () => {
     // layout is an array of objects, see the demo for more complete usage
@@ -24,23 +26,31 @@ const  competence  = () => {
     //];
     return (
         <div id='Competences' className={styles.Competence}>
-            <div key="r" className={styles.Git}>
+            <div className={styles.Git}>
                 <h2>Git and hosting services</h2>
                 <ul> 
-                    <li><img src={git}   alt='git'/> <p>GIT</p></li>
+                    <li><img src={git}      alt='git'/> <p>GIT</p></li>
                     <li><img src={gitlab}   alt='gitlab'/> <p>gitlab</p></li>
                     <li><img src={github}   alt='github'/><p>github</p></li>  
                 </ul>              
             </div>
-            <div key="b" className={styles.Basic}>
+            <div className={styles.Basic}>
                 <h2>Basic Web Developpment Languages</h2>
                 <ul> 
                     <li><img src={HTML5}  alt='HTML5'/></li>
                     <li><img src={CSS3}  alt='CSS3'/></li>
                     <li><img src={JavaScript}   alt='JavaScript'/></li>
                 </ul>
-            </div>               
-            <div key="g" className={styles.Advanced}>
+            </div>       
+            <div className={styles.Other}>
+                <h2>Other Programming Languages</h2>
+                <ul> 
+                    <li><img src={java}  alt='Java'/> <p>Java</p></li>
+                    <li><img src={python}  alt='Python'/> <p>Python</p></li>
+                    <li><img src={wolfram}  alt='Wolfram'/> <p>Wolfram</p></li>
+                </ul>
+            </div>         
+            <div className={styles.Advanced}>
                 <h2>Libraries Related to<br/> JavaScript</h2>
                 <ul>
                     <li><img src={react}  alt='react'/> <p>React</p></li>
@@ -49,18 +59,19 @@ const  competence  = () => {
                     <li><img src={npm}  alt='npm'/> <p>npm</p> </li>
                 </ul>
             </div>
-            <div key="y" className={styles.Dependencies}>
+            <div className={styles.Dependencies}>
                 <h2>Dependencies / Packages Related to React</h2>
                 <ul>
                     <li> <p>create-react-app</p></li>
-                    <li> <p>react-parallax</p></li>
-                    <li> <p>react-axios</p></li>
-                    <li> <p>react-css-variables</p></li>
-                    <li> <p>react-draggable</p></li>
+                    <li> <p>react-router</p></li>
                     <li> <p>react-loadable</p></li>
-                    <li> <p>react-grid-layout</p></li>
+                    <li> <p>react-redux</p></li>
+                    <li> <p>react-axios</p></li>
                     <li> <p>react-custom-scrollbars</p></li>
-                    <li> <p>react-router-dom</p></li>
+                    <li> <p>react-css-variables</p></li>
+                    <li> <p>react-parallax</p></li>
+                    <li> <p>react-draggable</p></li>
+                    <li> <p>react-grid-layout</p></li>
                     <li> <p>react-lazyload</p></li>
                 </ul>
             </div>    
