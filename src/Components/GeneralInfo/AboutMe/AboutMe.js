02128a01a4af2af9b49a10dc450bd47c02fb5082
Othmani Ali <othmanixali@gmail.com>
2018-05-23 09:47:47 +0100

    text fix in AboutMe cmp

--- src/Components/GeneralInfo/AboutMe/AboutMe.js
+++ src/Components/GeneralInfo/AboutMe/AboutMe.js
@@ -11,13 +11,13 @@ const aboutMe = () => {
                 <li><span><LogoFragment2 h={300}/>
                 </span>I'm a React Js web developer and a mechatronic engineer.</li>
                 <li><span><LogoFragment2 h={300}/>
-                </span> I have been interested in programming since my high school years and, the fact 
-                that I was a mechatronic engineer, I haven't the chance to know about web development until
-                I graduated. </li>
+                </span>Since I was a mechatronic engineer, I hadn't the oportunity and time to know about 
+                web development until I graduated. </li>
                 <li><span><LogoFragment2 h={300}/>
-                </span>As a self-taught person I had the chance to broaden my horizon by studying web development  
-                and also exploring Python and other non-web-only languages. This passion has since lasted and lead 
-                to my decision of becoming a web developer.</li>
+                </span>As a self-taught person, I had the chance to broaden my horizon by studying web development  
+                and also exploring Python and other non-web-only languages. I have been interested in programming 
+                since my high school years. This passion has since lasted and lead to my decision of becoming a web
+                developer.</li>
                 <li><span><LogoFragment2 h={300}/>
                 </span>The first step toward achieving my goal is to have a <strong>permanent job</strong> as a <b>Full-Stack Web Developer</b>.</li>
                 <li><span><LogoFragment2 h={300}/></span>This site will mainly show you details related to my experience as a web developer. If you are interrested to know further information about me, please visit my <a target="_blank" rel="noopener noreferrer" href='https://www.linkedin.com/in/ali-othmani-11873707/'>linkedin</a> profile.</li>
