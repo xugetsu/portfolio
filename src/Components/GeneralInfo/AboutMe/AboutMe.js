f632ba4c5f1fcb5c9bfbfbd60a21839c33b90795
Othmani Ali <othmanixali@gmail.com>
2018-05-23 01:15:24 +0100

    M text in AboutMe

--- src/Components/GeneralInfo/AboutMe/AboutMe.js
+++ src/Components/GeneralInfo/AboutMe/AboutMe.js
@@ -8,14 +8,22 @@ const aboutMe = () => {
             {/* <div className= {styles.SkewMe}></div> */}
             <h1>About Me</h1>
             <ul>
-                <li><span><LogoFragment2 h={300}/></span>I'm a React Js web developer and a mechatronic engineer.</li>
-                <li><span><LogoFragment2 h={300}/></span>I studied at the National School of Engineering of Sousse as a mechatronic engineer and, after graduation, I started learning about web development.</li>
-                <li><span><LogoFragment2 h={300}/></span>I was, and still am, very passionate about web development. This passion lead to my firm decision to keep going in this career path believing that, someday, I can benefit from both of my specialities together.</li>
-                <li><span><LogoFragment2 h={300}/></span>The first step toward achieving my goal is to have a <strong>permanent job</strong> as a <b>Full-Stack Web Developer</b>.</li>
+                <li><span><LogoFragment2 h={300}/>
+                </span>I'm a React Js web developer and a mechatronic engineer.</li>
+                <li><span><LogoFragment2 h={300}/>
+                </span> I have been interested in programming since my high school years and, the fact 
+                that I was a mechatronic engineer, I haven't the chance to know about web development until
+                I graduated. </li>
+                <li><span><LogoFragment2 h={300}/>
+                </span>As a self-taught person I had the chance to broaden my horizon by studying web development  
+                and also exploring Python and other non-web-only languages. This passion has since lasted and lead 
+                to my decision of becoming a web developer.</li>
+                <li><span><LogoFragment2 h={300}/>
+                </span>The first step toward achieving my goal is to have a <strong>permanent job</strong> as a <b>Full-Stack Web Developer</b>.</li>
                 <li><span><LogoFragment2 h={300}/></span>This site will mainly show you details related to my experience as a web developer. If you are interrested to know further information about me, please visit my <a target="_blank" rel="noopener noreferrer" href='https://www.linkedin.com/in/ali-othmani-11873707/'>linkedin</a> profile.</li>
             </ul>
         </div>
-    );
+    );//believing that, someday, I may benefit from both of my specialities together.
 }
 
 export default aboutMe;
