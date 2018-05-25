6a72d8ad977c4ab3e1f4a5bc33349d4b28254e48
Othmani Ali <othmanixali@gmail.com>
2018-05-25 15:59:51 +0100

    typo fix

--- src/Components/MyCareer/CommitDetails/Commits/w/06.js
+++ src/Components/MyCareer/CommitDetails/Commits/w/06.js
@@ -9,7 +9,7 @@ const commit = (props) => {
     return (
         <CustomScrollbars scrollId={props.hash} width = {'100%'} height={'450px'} zindex={2} >
             <article>
-                <p>It is time to dive deeper into web development by learning React.
+                <p>It is time to dive deeper into the web's awesomeness and broaden my horizon by learning React !
                 <br />
                 <div className={styles.ReactLogoWrapper}>
                     <img height='200px' src={research} alt='research'/>
@@ -18,11 +18,11 @@ const commit = (props) => {
                  <br />React is a powerful JavaScript library ideal for building interactive, 
                 data-driven user interfaces, and it’s used by some of the most successful brands in the world, 
                 including Facebook, Netflix, Airbnb, and more.</p>
-                <p>I Started learning React Fundamentals by taking the official React site's tutorial: <a href='https://reactjs.org/tutorial/tutorial.html' target="_blank" rel="noopener noreferrer"> Intro To React </a>
-                and later I dived deeper into React's advanced features by taking <a href='https://reactjs.org/tutorial/tutorial.html' target="_blank" rel="noopener noreferrer"> React 16 - The Complete Guide </a>
-                course which also includes 
+                <p>I Started learning React Fundamentals with the provided tutorial in the official React's site : <a href='https://reactjs.org/tutorial/tutorial.html' target="_blank" rel="noopener noreferrer"> Intro To React </a>
+                and later I dived deeper into React's advanced features by taking <a href='https://www.udemy.com/react-the-complete-guide-incl-redux' target="_blank" rel="noopener noreferrer"> the React 16 - The Complete Guide </a>
+                course which also includes working with the well known dependencies like
                 <b><Anchor href='https://reacttraining.com/react-router/' color={'#55b2ff'}> Router 4 </Anchor></b> and 
-                <b><Anchor href='https://redux.js.org/' color={'#a557ff'}> Redux.js </Anchor></b>.
+                <b><Anchor href='https://redux.js.org/' color={'#b97dfd'}> Redux.js </Anchor></b> .
                 </p>
             </article>
         </CustomScrollbars> 
