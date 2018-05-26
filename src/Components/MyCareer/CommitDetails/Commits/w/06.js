2c20ebcd6a5b6fc8fe6509a94dad66e72f37612b
Othmani Ali <othmanixali@gmail.com>
2018-05-26 15:44:15 +0100

    bug fix

--- src/Components/MyCareer/CommitDetails/Commits/w/06.js
+++ src/Components/MyCareer/CommitDetails/Commits/w/06.js
@@ -9,20 +9,21 @@ const commit = (props) => {
     return (
         <CustomScrollbars scrollId={props.hash} width = {'100%'} height={'450px'} zindex={2} >
             <article>
-                <p>It is time to dive deeper into the web's awesomeness and broaden my horizon by learning React !
+                <p>It is time to dive deeper into the web's awesomeness and broaden my horizon by learning React !</p>
                 <br />
                 <div className={styles.ReactLogoWrapper}>
                     <img height='200px' src={research} alt='research'/>
                     <img className={styles.ReactLogo} src={logo} alt='logo'/>
                 </div>
-                 <br />React is a powerful JavaScript library ideal for building interactive, 
-                data-driven user interfaces, and it’s used by some of the most successful brands in the world, 
-                including Facebook, Netflix, Airbnb, and more.</p>
+                <p>React is a powerful JavaScript library ideal for building interactive, 
+                    data-driven user interfaces, and it’s used by some of the most successful brands in the world, 
+                    including Facebook, Netflix, Airbnb, and more.
+                </p>
                 <p>I Started learning React Fundamentals with the provided tutorial in the official React's website : <a href='https://reactjs.org/tutorial/tutorial.html' target="_blank" rel="noopener noreferrer"> Intro To React</a>.
-                 <br/>Later I dived deeper into the advanced features of React by taking the<a href='https://www.udemy.com/react-the-complete-guide-incl-redux' target="_blank" rel="noopener noreferrer"> React 16 - The Complete Guide </a>
-                course including working with the well known dependencies like
-                <b><Anchor href='https://reacttraining.com/react-router/' color={'none'}> Router 4 </Anchor></b> and 
-                <b><Anchor href='https://redux.js.org/' color={'none'}> Redux.js </Anchor></b> .
+                    <br/>Later I dived deeper into the advanced features of React by taking the<a href='https://www.udemy.com/react-the-complete-guide-incl-redux' target="_blank" rel="noopener noreferrer"> React 16 - The Complete Guide </a>
+                    course including working with the well known dependencies like
+                    <b><Anchor href='https://reacttraining.com/react-router/' color={'none'}> Router 4 </Anchor></b> and 
+                    <b><Anchor href='https://redux.js.org/' color={'none'}> Redux.js </Anchor></b> .
                 </p>
             </article>
         </CustomScrollbars> 
